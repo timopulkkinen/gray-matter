7915d954fde6c91bcdf3e52c73be4553546886ab
Timo Pulkkinen <timo.pulkkinen@cemron.com>
2017-04-26 17:17:40 +0300

    added browserified build

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
 
 var lint = ['index.js', 'lib/*.js', 'test/*.js'];
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var derequire = require('gulp-derequire');
+var uglify = require('gulp-uglify');
 
 gulp.task('coverage', function () {
   return gulp.src(lint)
@@ -17,6 +22,20 @@ gulp.task('mocha', ['coverage'], function () {
   return gulp.src('test/*.js')
     .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports());
+});
+
+gulp.task('browserify', function() {
+  var b = browserify({
+    standalone: 'index',
+    entries: './index.js',
+    debug: false,
+  });
+  return b.bundle()
+   .pipe(source('index.js'))
+   .pipe(derequire())
+   .pipe(buffer())
+   .pipe(uglify())
+   .pipe(gulp.dest('./lib/browserify/'))
 });
 
 gulp.task('eslint', function () {
